d3a12e2fda3b04963eb47cf57b1eac48d20373f4
ljh-c <57099181+ljh-c@users.noreply.github.com>
2019-12-25 14:14:36 -0800

    adds TodoForm

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function TodoForm() {
+  return (
+    <form>
+      <input type="text" placeholder="Add new task" />
+      <button>&#43;</button>
+    </form>
+  );
+}
